7c62ca48aaaed334531f2034fe5b829dec6b7242
David Holland <davidzholland@gmail.com>
2020-10-15 17:45:43 -0500

    Sort files by track number in meta data and remove track from file names

--- copySortedFAT.py
+++ copySortedFAT.py
@@ -4,6 +4,7 @@ import glob
 import re
 import time
 import shutil
+import functools
 from modules.tinytag import TinyTag
 
 
@@ -15,7 +16,6 @@ def main():
 
 def copy_full_directory(source, destination, source_directory):
     child_directories = get_all_subdirectories(source_directory)
-    print('child_directories: ', child_directories)
     # Recursively copy child directories
     for child_directory in child_directories:
         # Concatenate directory names to form one flat directory for this folder
@@ -25,8 +25,6 @@ def copy_full_directory(source, destination, source_directory):
     
 def recursively_copy_directory_and_children(source, destination, source_directory):
     copy_fat_directory(source_directory, source_directory.replace(source, destination))
-    # TODO: Option to convert nested directories into one flat level of directories & concatenate artist + album
-    # 
     child_directories = get_all_subdirectories(source_directory)
     # Recursively copy child directories
     for child_directory in child_directories:
@@ -60,30 +58,55 @@ def get_files(path):
             files.append(path)
     return files
 
+def compare_by_track_natural(item1, item2):
+    # Primary sort by track number meta data
+    if TinyTag.get(item1).track is None and TinyTag.get(item2).track is not None:
+        return -1
+    elif TinyTag.get(item1).track is not None and TinyTag.get(item2).track is None:
+        return 1
+    elif TinyTag.get(item1).track is not None and TinyTag.get(item2).track is not None:
+        if int(TinyTag.get(item1).track) > int(TinyTag.get(item2).track):
+            return 1
+        elif int(TinyTag.get(item1).track) < int(TinyTag.get(item2).track):
+            return -1
+        else:
+            return 0
+    else:
+        # Secondary sort by file name (natural sort)
+        sorted_list = natural_sort([item1, item2])
+        if sorted_list[0] == item1:
+            return -1
+        else:
+            return 1
+
+def sort_by_track_and_filename(source_files):
+    return sorted(source_files, key=functools.cmp_to_key(compare_by_track_natural))
+
 def copy_fat_directory(source, destination):
-    print('source: ', source)
-    print('destination: ', destination)
+    print('copy_fat_directory: ', source, destination)
     source_files = get_files(source)
-    # print('source_files: ', source_files)
-    # TODO: Option to primary sort by track number meta data, secondary by file name
-    for source_file in source_files:
-        tag = TinyTag.get(source_file)
-        print(source_file, tag.track)
-    sorted_source_files = natural_sort(source_files)
+    if len(source_files) <= 0:
+        return
+    sorted_source_files = sort_by_track_and_filename(source_files)
+    # exit()
     for source_file_path in sorted_source_files:
-        # Define destination file path
+        # Define destination file path and use original file name
         destination_file_path = source_file_path.replace(source, destination)
+        # Remove track number from file name
+        if TinyTag.get(source_file_path).track is not None:
+            track_characters = len(str(TinyTag.get(source_file_path).track))
+            if (
+                os.path.basename(destination_file_path)[:track_characters + 1] == str(TinyTag.get(source_file_path).track) + ' '
+                or os.path.basename(destination_file_path)[:track_characters + 2] == '0' + str(TinyTag.get(source_file_path).track) + ' '
+            ):
+                destination_file_path = os.path.dirname(destination_file_path) + os.path.sep + re.sub('^[0-9]+\s(\-\s)?', '', os.path.basename(destination_file_path))
         # Create directory if doesn't exist
         os.makedirs(os.path.dirname(destination_file_path), exist_ok=True)
         # Copy file
-        print('Copying: ', source_file_path)
+        print('Copying to: ', destination_file_path)
         shutil.copy(source_file_path, destination_file_path)
         # Iterate files with a 2.1s pause
         time.sleep(2.1)
-
-def natural_track_sort(l):
-    # TODO: 
-    pass
 
 def natural_sort(l): 
     convert = lambda text: int(text) if text.isdigit() else text.lower() 
